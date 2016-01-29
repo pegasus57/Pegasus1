614f0d5fa1aa8554e1553b8ba5bfa742e3c1602d
pegasus57 <ruiiiyang@outlook.com>
2016-01-29 15:45:26 -0500

    initial commit

---
+++ unity3d/Assets/_Scripts/capsuleScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class capsuleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Debug.Log("In the Start Method");
+	
+	}
+    //// FixedUpdate Method i for Physics
+    //void FixedUpdate() {
+    //    Debug.Log("Fixed Update");
+        
+    //}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Update");
+	}
+}
